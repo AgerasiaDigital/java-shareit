c256ec5298576569632a64e1f49a49d8c0d71a30
Oleg Digital <boss@agerasiadigital.ru>
2025-11-14 09:16:57 +0300

    Удалил импорты

--- server/src/test/java/ru/practicum/shareit/booking/BookingTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingTest.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 
 class BookingTest {
 
