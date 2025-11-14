054746a648b7a14be436a1040ad4830c631e8d20
Oleg Digital <boss@agerasiadigital.ru>
2025-11-14 08:34:17 +0300

    Добавил больше тестов

---
+++ server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,57 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void createUser_shouldReturn201() throws Exception {
+        UserDto userDto = new UserDto(null, "Test User", "test@email.com");
+        UserDto responseDto = new UserDto(1L, "Test User", "test@email.com");
+
+        when(userService.createUser(any())).thenReturn(responseDto);
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Test User"))
+                .andExpect(jsonPath("$.email").value("test@email.com"));
+    }
+
+    @Test
+    void getAllUsers_shouldReturn200() throws Exception {
+        UserDto userDto = new UserDto(1L, "Test User", "test@email.com");
+
+        when(userService.getAllUsers()).thenReturn(List.of(userDto));
+
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1));
+    }
+}
